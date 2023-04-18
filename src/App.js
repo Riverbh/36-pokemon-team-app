330e9fcdfe1cf44f0c6f3fc1deb6c10680e97e18
River Hansen <riverbh01@gmail.com>
2023-04-18 10:04:34 -0600

    first commit with header components and screen components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header';
+import { useState } from 'react'
+import TeamScreen from './Screens/TeamScreen';
+import DexScreen from './Screens/DexScreen';
 
 function App() {
+  const [teamPage, setTeamPage] = useState(false)
+
+  const changePage = (value) => {
+    if(teamPage === value){
+      return
+    }
+    setTeamPage(value)
+  }
+
+  console.log("CURRENT PAGE", teamPage)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='row-container'>
+        <button onClick={() => changePage(true)}>Team</button>
+        <button onClick={() => changePage(false)}>Dex</button>
+      </div>
+      {teamPage ? <TeamScreen /> : <DexScreen/>}
     </div>
   );
 }
---
+++ src/Screens/DexScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DexScreen = () => {
+    return (
+        <div>
+            <h1>PokeDex</h1>
+        </div>
+    )
+}
+
+export default DexScreen
