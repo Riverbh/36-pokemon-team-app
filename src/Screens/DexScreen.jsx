784ec86f62fa61aecb2c8e2e558e05dac130a640
River Hansen <riverbh01@gmail.com>
2023-04-19 10:00:19 -0600

    added images api to the the pokedex and added a functional search bar

--- src/Screens/DexScreen.jsx
+++ src/Screens/DexScreen.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import PokedexCard from "../Components/PokedexCard";
 
 const DexScreen = () => {
     const [pokemon, setPokemon] = useState([])
+    const [search, setSearch] = useState('')
+
+    const handleSearch = (e) => setSearch(e.target.value.toLowerCase())
 
     useEffect(()=>{
         axios
         .get(`https://pokeapi.co/api/v2/pokemon?limit=1000`)
         .then((res) => {
-            setPokemon(res.data.results)
+            let newState = res.data.results.map((item, index) => {
+                item.img=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+                return item
+            })
+            setPokemon(newState)
+            console.log(newState)
         })
     }, [])
 
-    const pokemonDisplay = pokemon.map((poke, index) => {
-        return <h4>{poke.name}</h4>
+    const pokemonDisplay = pokemon
+        .filter((poke, index) => {
+            return poke.name.includes(search)
+        })
+        .map((poke, index) => {
+        return <PokedexCard poke={poke} index={index}/>
     })
 
     return (
         <div>
             <h1>PokeDex</h1>
+            <input type="text" placeholder="Search" onChange={handleSearch}/>
             {pokemonDisplay}
         </div>
     )
